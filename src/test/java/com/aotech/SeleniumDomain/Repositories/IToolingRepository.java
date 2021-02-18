05f9460a915884e24e9036883b88f2f9df84227f
Alexandre Ortigosa <alexandre.ortigosa@schibsted.com>
2021-02-18 13:59:21 +0100

    making the repository

--- src/test/java/com/aotech/SeleniumDomain/Repositories/IToolingRepository.java
+++ src/test/java/com/aotech/SeleniumDomain/Repositories/IToolingRepository.java
@@ -1,4 +1,10 @@
 package com.aotech.SeleniumDomain.Repositories;
 
-public interface IToolingRepository {
+public interface IToolingRepository<T> {
+    String getTitle(T filter);
+    void clickButton(T filter);
+    void setInput(T filter);
+    void executeScript(T filter, String script);
+    void acceptDialog();
+    void sleep();
 }
--- src/test/java/com/aotech/SeleniumDomain/Repositories/SeleniumToolingRepository.java
+++ src/test/java/com/aotech/SeleniumDomain/Repositories/SeleniumToolingRepository.java
@@ -2,4 +2,33 @@ package com.aotech.SeleniumDomain.Repositories;
 
 public class SeleniumToolingRepository implements IToolingRepository {
 
+    @Override
+    public String getTitle(Object filter) {
+        return null;
+    }
+
+    @Override
+    public void clickButton(Object filter) {
+
+    }
+
+    @Override
+    public void setInput(Object filter) {
+
+    }
+
+    @Override
+    public void executeScript(Object filter, String script) {
+
+    }
+
+    @Override
+    public void acceptDialog() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
 }
